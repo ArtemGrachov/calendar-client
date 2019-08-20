13bb0c48355e1a67ba34a640f3a10819302d8241
Artem Grachov <grachov2195@gmail.com>
2019-08-20 20:07:06 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div id="app">
+        <h1>Hello, world</h1>
+        <hr />
+        <router-view></router-view>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'root',
+      get component() {
+        const isAuth = false;
+
+        if (isAuth) {
+          return () => import(/* webpackChunkName: "user" */ './views/ViewUser.vue');           
+        } else {
+          return () => import(/* webpackChunkName: "guest" */ './views/ViewGuest.vue');
+        }
+      }
+    }
+  ]
+})
