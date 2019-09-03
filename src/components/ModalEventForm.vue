78165bac4a281b5c6a4f2bbf44b817bc93578e7f
Artem Grachov <grachov2195@gmail.com>
2019-09-03 20:28:13 +0300

    Confirmation on closing edit form

--- src/components/ModalEventForm.vue
+++ src/components/ModalEventForm.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="modal-backdrop" @click.self="close">
+    <div class="modal-backdrop" @click.self="closeConfirmation">
         <div class="modal">
             <div class="modal-content d-flex justify-content-between">
                 <div class="title">New event</div>
                 <button
-                    @click="close"
+                    @click="closeConfirmation"
                     class="button-round"
                 >
                     <i class="material-icons">
@@ -25,11 +25,28 @@
 import FormEvent from './FormEvent';
 import modalWindowMixin from '../mixins/modal-window-mixin';
 import { FORM_ACTIONS_SUCCESS } from '../store/form/action-types';
+import Modal from '../modal';
+import ModalConfirmation from './ModalConfirmation';
 
 export default {
     mixins: [modalWindowMixin],
     components: {
         FormEvent
+    },
+    methods: {
+        async closeConfirmation() {
+            const confirmation = await Modal.openModal({
+                data: {
+                    question: 'Are you sure you want to close edit form?'
+                },
+                component: ModalConfirmation
+            });
+
+            if (confirmation) {
+                this.close();
+            }
+
+        }
     },
     created() {
         this.$options.actionSub = this.$store.subscribeAction((action) => {
