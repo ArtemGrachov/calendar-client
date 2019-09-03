bb6ad36f30e827c5e1964c5e2fa1ba5ab4b90c79
Artem Grachov <grachov2195@gmail.com>
2019-09-03 17:50:38 +0300

    Confirmation modal

---
+++ src/components/ModalConfirmation.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="modal-backdrop">
+        <div class="modal">
+            <div class="modal-content d-flex justify-content-between">
+                <div class="mb-3">
+                    {{ data.question }}
+                </div>
+                <button
+                    class="button-success"
+                    @click="confirm"
+                >
+                    Yes
+                </button>
+                <button
+                    class="button-danger"
+                    @click="reject"
+                >
+                    No
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import modalWindowMixin from '../mixins/modal-window-mixin';
+
+export default {
+    mixins: [modalWindowMixin],
+    methods: {
+        confirm() {
+            this.resolve(true);
+            this.close();
+        },
+        reject() {
+            this.resolve(false);
+            this.close();
+        }
+    }
+}
+</script>
