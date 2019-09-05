0b456cdc38a702d248c198a6313d0e1673103ebb
Artem Grachov <grachov2195@gmail.com>
2019-09-05 17:38:04 +0300

    Calendar nav responsive markup

--- src/components/CalendarNav.vue
+++ src/components/CalendarNav.vue
@@ -1,8 +1,8 @@
 <template>
-    <nav class="py-2 px-3">
+    <nav class="px-3 mb-5">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-lg-4 mb-3 mb-lg-0">
+                <div class="col-12 col-lg-4 mb-3 mb-lg-0 left">
                     <button
                         class="button-transparent button-round"
                         @click="prev"
@@ -12,7 +12,7 @@
                         </i>
                     </button>
                     <button
-                        class="button-transparent mx-3"
+                        class="button-transparent mx-1 mx-sm-3"
                         @click="today"
                     >
                         Today
@@ -25,28 +25,42 @@
                             keyboard_arrow_right
                         </i>
                     </button>
+                    <div class="d-md-none mt-3">
+                        <button
+                            class="button-transparent button-round toggle-mode-controls"
+                            :class="{ show: showMode }"
+                            @click="toggleShowControls"
+                        >
+                            <i class="material-icons">
+                                keyboard_arrow_down
+                            </i>
+                        </button>
+                    </div>
                 </div>
-                <div class="col-12 col-lg-8 text-right">
+                <div
+                    class="col-12 col-lg-8 right mode-controls"
+                    v-show="showMode || !isMd"
+                >
                     <button
-                        class="button-transparent button-grey mx-1"
+                        class="mode-button button-transparent button-grey mx-md-1 my-2 my-md-0"
                         @click="changeMode('day')"
                     >
                         Day
                     </button>
                     <button
-                        class="button-transparent button-grey mx-1"
+                        class="mode-button button-transparent button-grey mx-md-1 my-2 my-md-0"
                         @click="changeMode('week')"
                     >
                         Week
                     </button>
                     <button
-                        class="button-transparent button-grey mx-1"
+                        class="mode-button button-transparent button-grey mx-md-1 my-2 my-md-0"
                         @click="changeMode('month')"
                     >
                         Month
                     </button>
                     <button
-                        class="button-transparent button-grey mx-1"
+                        class="mode-button button-transparent button-grey mx-sm-1"
                         @click="changeMode('year')"
                     >
                         Year
@@ -59,6 +73,12 @@
 
 <script>
 export default {
+    data() {
+        return {
+            showMode: false,
+            isMd: false
+        }
+    },
     methods: {
         changeMode(mode) {
             this.$emit('mode', mode);
@@ -71,7 +91,60 @@ export default {
         },
         today() {
             this.$emit('today');
+        },
+        toggleShowControls() {
+            this.showMode = !this.showMode;
         }
+    },
+    created() {
+        const cmp = this;
+
+        this.$options.widthListener = function() {
+            if (window.innerWidth >= 768) {
+                cmp.isMd = false;
+            } else {
+                cmp.isMd = true;
+            }
+        }
+
+        window.addEventListener('resize', this.$options.widthListener);
+    },
+    destroyed() {
+        window.removeEventListener('resize', this.$options.widthListener);
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    @import '../styles/variables';
+
+    .right {
+        text-align: center;
+    }
+
+    .left {
+        text-align: center;
+    }
+
+    .mode-button {
+        display: block;
+        width: 100%;
+
+        @media (min-width: 768px) {
+            width: auto;
+            display: inline-block;
+        }
+    }
+
+    .material-icons {
+        transition: $transition;
+    }
+
+    .toggle-mode-controls {
+        &.show {
+            .material-icons {
+                transform: rotate(180deg);
+            }
+        }
+    }
+</style>
