9f58441b2f9d454cc2c8ff225c4a54bc0f6b7392
Artem Grachov <grachov2195@gmail.com>
2019-09-07 22:39:02 +0300

    Real-time updating events, alerts, notifications

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,5 +1,9 @@
-import { USER_ACTIONS_CLEAR } from "../store/user/action-types";
-import { FORM_ACTIONS_SUCCESS } from "../store/form/action-types";
+import { USER_ACTIONS_CLEAR } from '../store/user/action-types';
+import { FORM_ACTIONS_SUCCESS } from '../store/form/action-types';
+import { WS_EVENT_UPDATED, WS_EVENT_DELETED, WS_USER_INVITED_TO_EVENT, WS_USER_REMOVED_FROM_EVENT, WS_USER_LEAVE_EVENT } from '../config/ws-events';
+import { NOTIFICATIONS_ACTIONS_ADD_NOTIFICATION } from '../store/notifications/action-types';
+import { EVENTS_ACTIONS_UPSERT_EVENT, EVENTS_ACTIONS_STORE_DELETE_EVENT, EVENTS_ACTIONS_ADD_EVENT, EVENTS_ACTIONS_STORE_REMOVE_USER_FROM_EVENT } from '../store/events/action-types';
+import { ALERTS_ACTIONS_CREATE_ALERT } from '../store/alerts/action-types';
 
 export default class WebSocketClient {
     path = null;
@@ -33,20 +37,16 @@ export default class WebSocketClient {
 
         this.socket = new WebSocket(`${this.path}?token=${token}`);
 
-        this.socket.onopen = () => {
-            console.log('Connection established!');
-        }
-
         this.socket.onerror = () => {
-            console.log('Error!');
+            console.error('WebSocket error');
         }
 
-        this.socket.onmessage = ev => {
-            console.log('New message', ev);
+        this.socket.onmessage = event => {
+            this.onMessage(event);
         }
 
         this.socket.onclose = () => {
-            console.log('Connection closed');
+            console.warn('Connection closed');
         }
     }
 
@@ -55,6 +55,72 @@ export default class WebSocketClient {
             this.socket.close();
         } else {
             console.warn('Socket connecton was not initialized');
+        }
+    }
+
+    onMessage(event) {
+        const data = JSON.parse(event.data);
+
+        this.store.dispatch(
+            'notifications/' + NOTIFICATIONS_ACTIONS_ADD_NOTIFICATION,
+            { item: data.notification },
+            { root: true }
+        );
+
+        this.store.dispatch(
+            'alerts/' + ALERTS_ACTIONS_CREATE_ALERT,
+            {
+                title: data.notification.title,
+                description: data.notification.content,
+                type: data.notification.type
+            },
+            { root: true }
+        );
+
+        switch (data.type) {
+            case WS_EVENT_UPDATED: {
+                this.store.dispatch(
+                    'events/' + EVENTS_ACTIONS_UPSERT_EVENT,
+                    { item: data.event },
+                    { root: true }
+                );
+                break;
+            }
+            case WS_EVENT_DELETED: {
+                this.store.dispatch(
+                    'events/' + EVENTS_ACTIONS_STORE_DELETE_EVENT,
+                    { id: data.eventId   },
+                    { root: true }
+                );
+                break;
+            }
+            case WS_USER_INVITED_TO_EVENT: {
+                this.store.dispatch(
+                    'events/' + EVENTS_ACTIONS_ADD_EVENT,
+                    { item: data.event },
+                    { root: true }
+                );
+                break;
+            }
+            case WS_USER_REMOVED_FROM_EVENT: {
+                this.store.dispatch(
+                    'events/' + EVENTS_ACTIONS_STORE_DELETE_EVENT,
+                    { id: data.eventId   },
+                    { root: true }
+                );
+                break;
+            }
+            case WS_USER_LEAVE_EVENT: {
+                this.store.dispatch(
+                    'events/' + EVENTS_ACTIONS_STORE_REMOVE_USER_FROM_EVENT,
+                    {
+                        eventId: data.eventId,
+                        userId: data.userId
+                    },
+                    { root: true }
+                );
+                break;
+            }
         }
     }
 }
--- src/store/notifications/action-types.js
+++ src/store/notifications/action-types.js
@@ -1,2 +1,3 @@
 export const NOTIFICATIONS_ACTIONS_GET_NOTIFICATIONS = '[Notifications actions] Get notifications';
 export const NOTIFICATIONS_ACTIONS_DELETE_NOTIFICATION = '[Notifications actions] Delete notification';
+export const NOTIFICATIONS_ACTIONS_ADD_NOTIFICATION = '[Notifications actions] Add notification';
