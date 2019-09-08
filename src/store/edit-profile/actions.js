845ba46ae28c19c275b3fdb6f227a0d8616ca1e8
Artem Grachov <grachov2195@gmail.com>
2019-09-09 00:31:22 +0300

    User avatar updates

--- src/store/edit-profile/actions.js
+++ src/store/edit-profile/actions.js
@@ -1,6 +1,12 @@
-import { FORM_ACTIONS_SUCCESS } from '../form/action-types';
+import {
+    FORM_ACTIONS_SUCCESS,
+    FORM_ACTIONS_FAIL
+} from '../form/action-types';
 import { USER_ACTIONS_SET_DATA } from '../user/action-types';
-import { ALERTS_ACTIONS_SUCCESS } from '../alerts/action-types';
+import {
+    ALERTS_ACTIONS_SUCCESS,
+    ALERTS_ACTIONS_DANGER
+} from '../alerts/action-types';
 
 export default {
     [FORM_ACTIONS_SUCCESS](context, payload) {
@@ -8,6 +14,13 @@ export default {
         context.dispatch(
             'alerts/' + ALERTS_ACTIONS_SUCCESS,
             { description: payload.message },
+            { root: true }
+        );
+    },
+    [FORM_ACTIONS_FAIL](context, payload) {
+        context.dispatch(
+            'alerts/' + ALERTS_ACTIONS_DANGER,
+            { description: payload.data.message },
             { root: true }
         );
     }
